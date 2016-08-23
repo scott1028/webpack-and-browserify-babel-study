1c05fe59dff4b323c94d6c2010c72dda7ed94f7f
scott.lan <scott.lan@taisys.com>
2016-08-23 17:42:31 +0800

    update sample

--- project_templates/app/app.js
+++ project_templates/app/app.js
@@ -1,5 +1,5 @@
 var lib01 = require('./lib/lib_01.js');
-var jquery = require('jquery');
+// var jquery = require('jquery');
 // var lib02 = require('lib_02');  // in ./node_modules/lib_02.js, this is code convention.
 // var lib03 = require('jquery');  // like <script src="./jquery.js"></script> in html file
 
@@ -10,5 +10,18 @@ console.log(lib01.a);
 // console.log(lib03('window'));
 // console.log(lib03('body').text('hello webpack with jQuery'));
 console.log(31232444);
-console.log(jquery('div'));
-jquery('body').html('test text by jquery');
+// console.log(jquery('div'));
+
+
+function mainController(){
+	var jquery = require('jquery');
+	jquery('body').html('test text by jquery#1');
+	main2Controller();
+};
+
+function main2Controller(){
+	var jquery2 = require('jquery');
+	jquery2('body').append('<div>test text by jquery#2</div>');
+}
+
+mainController();
