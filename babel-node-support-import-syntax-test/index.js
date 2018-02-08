1645e00e1e9855d9ee61fdaed0d79db5bf14b983
scott <scott.lan@taisys.com>
2018-02-08 19:52:23 +0800

    update code

--- babel-node-support-import-syntax-test/index.js
+++ babel-node-support-import-syntax-test/index.js
@@ -1,3 +1,6 @@
 import path from 'path';
+import {a, b} from './lib';
+import c from './lib';
 
 console.log(path);
+console.log(a, b, c);
---
+++ babel-node-support-import-syntax-test/lib.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// for import {a, b} from './lib';
+export var a = 1;
+export var b = 2;
+
+// for import c from './lib';
+export default {
+	a,
+	b
+}
